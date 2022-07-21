2085ced4f1e99b1ddbd1966c48886a7d6567f258
revathypcm25 <deepaelango1204@gmail.com>
2022-07-21 16:07:58 +0530

    coding

---
+++ src/com/collections/Ques6Sec4.java
@@ -0,0 +1,32 @@
+package com.collections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ques6Sec4 {
+	
+	public static void main(String[] args) {
+		
+		// creating a list
+	
+		
+//		 Get the each value of list by using normal for loop
+//        Input:   List = 105,205,305,405,505,605,705,805
+		
+		List<Integer> li = new LinkedList<Integer>();
+	
+	li.add(105);
+	li.add(205);
+	li.add(305);
+	li.add(405);
+	li.add(505);
+	li.add(605);
+	li.add(705);
+	li.add(805);
+	
+	System.out.println(li);
+	
+	
+	}
+
+}
